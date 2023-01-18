5f80de8cfe3768ac5f08fa75d69b7d2c523b1db3
DylanAguirreOspina <85912090+DylanAguirreOspina@users.noreply.github.com>
2023-01-17 20:17:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//https://teachablemachine.withgoogle.com/models/[...]
+
+function startClassification(){
+navigator.mediaDevices.getUserMedia({audio:true});
+classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/[...]")
+
+
+}
+function modelReady(){
+classifier.classify(gotResults);
+
+
+}
+function gotResults(error,results){
+    console.log("Got Result!!!");
+}
